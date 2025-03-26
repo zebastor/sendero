2110bbd65c0630293b7353d6494fcdf81865d093
ElSebastianBarreto <sebastian.barreto.rojas@unillanos.edu.co>
2025-03-26 18:34:21 -0500

    inicio

---
+++ src/main/java/unillanos/sendero/SenderoApplication.java
@@ -0,0 +1,13 @@
+package unillanos.sendero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderoApplication.class, args);
+	}
+
+}
